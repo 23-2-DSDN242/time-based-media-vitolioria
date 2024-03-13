8a62b4907b86df0f11fa2abed6d2459f9a3790b5
vitolioria <147349823+vitolioria@users.noreply.github.com>
2024-03-13 20:25:19 +1300

    Add files via upload

--- clock.js
+++ clock.js
@@ -1,8 +1,6 @@
 /*
  * use p5.js to draw a clock on a 960x500 canvas
  */
-
-
   // draw your own clock here based on the values of obj:
   //    obj.hours goes from 0-23
   //    obj.minutes goes from 0-59
@@ -19,109 +17,83 @@
   
   line(480,0,480,500);
   line(0,250,960,250);
-  
+   
+  push();
+  noFill();
+  ellipse(480,250,400); //clock reference
+  pop();
 
+  
+  let font;
   let hours = obj.hours;
   let minutes = obj.minutes;
   let seconds = obj.seconds;
   let millis = obj.millis;
   let alarm = obj.seconds_until_alarm;
-  let blue = color(122,255,215);
+
+  
+  
+ //seconds code
+ let blue = color(122,255,215);
   let orange = color(255,179,117);
   let lerpSeconds = map(obj.seconds,0,59,0,1);
   let secondsColour = lerpColor(blue,orange,lerpSeconds);
- 
- push();
- translate(480,250);
+  let secondsWithFraction = obj.seconds + (obj.millis / 999.0);
+  let secondRotateSmooth = map(secondsWithFraction, 0, 60, 0, 360);  
+
+  push();
+  translate(480,250);
   rotate(-90);
-  strokeWeight(15);
+  strokeWeight(20);
   stroke(secondsColour);
   noFill();
-  let end = map(seconds, 0, 60, 0, 360);
-  arc(0,0,470,470,0,end);
+  let end = map(secondRotateSmooth, 0, 360, 0, 360);
+  arc(0,0,300,300,0,end);
   pop();
 
-  // push();
+  //minutes code
+  minuteDigit = map(obj.minutes,0,59,0,59);
+  var minuteChange = 0;
+
+  fill(255,204,253);
+  textSize(200);
+  textFont('Roboto'); //google fonts
+  text(minuteDigit,365,320);
+
+  // if (obj.minutes < 10){
+  //   text("0",365,320,minuteDigit,365,320);
+  // }
+  //text(minuteDigit[minuteChange],370,320);
+   //minuteChange = minuteChange + 1;
+  // if (minutes == 59){
+  //   minuteChange = [0];
+  // }
+
+  if (hours > 18 || hours < 6){
+    background(255,243,168);
+    fill(20,54,219);
+    textSize(200);
+    textFont('Roboto'); //google fonts
+    text(minuteDigit,365,320);
+    
+   
+  }
+  //hours code
+  
+  push();
+  noStroke();
+  fill(222,255,204);
+  ellipse(480,50,40); //12th hour
+  //ellipse(580,85,40); //1st hour
  
-  //  strokeWeight(5);
-  //  stroke(122,255,215);
-  // //  noFill();
-  // //  let end = map(seconds, 0, 60, 0, 360);
-  //  arc(0,0,470,470,0,end);
-  //  pop();
+  ellipse(680,250,40); //3rd hour
+  ellipse(480,450,40);//6th hour
+  ellipse(280,250,40); //9th hour
 
-  // strokeWeight(8);
-  // stroke(255, 100, 150);
-  // noFill();
-  // ellipse(200,200,300,300);
-  // push();
-  // strokeWeight(4);
-  // noFill();
-  // ellipse(480,250,500) //size guide
-  // pop();
-
-
-  // //arc(0, 0, 300, 300, 0, secondAngle);
-  // push();
-  // rotate(secondAngle);
-  // stroke(255, 100, 150);
-  // line(0, 0, 100, 0);
-  // pop();
-
-
-  // angleMode(DEGREES);
-
+  
+  pop();
  
- 
-
- // let hourSize = 60;
-
-//   for (let i = 0; i <= 23; i++){
-// rotate(360/24)
-
-//     if (obj.hours == i){
-//       hourSize = 60;
-//     }
-//     else{
-//       hourSize = 60;
-//     }
-
-//     ellipse(100,100,20,hourSize);
-
-//   }
-
-//   push(); //12:00 circle
-//   noFill();
-//   stroke(255,106,0);
-//   strokeWeight(3);
-//   circle(480,50,60,60);
-//   pop();
-
-//   push(); //6:00 circle
-//   noFill();
-//   stroke(255,106,0);
-//   strokeWeight(3);
-//   circle(480,450,60,60);
-//   pop();
-
-//   push(); //3:00 circle
-//   noFill();
-//   stroke(255,106,0);
-//   strokeWeight(3);
-//   circle(680,250,60,60);
-//   pop();
-
-//   push(); //9:00 circle
-//   noFill();
-//   stroke(255,106,0);
-//   strokeWeight(3);
-//   circle(280,250,60,60);
-//   pop();
-
+  
 }
 
-//bellow to make the minute text to change back to 00 from 59
-// index = index + 1;
-//if (index == 60){
-  //  index = 0;
-//}
+
